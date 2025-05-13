857b8b9a5b4a408d74b6bfe4ce97b4e8aba8b83b
Sarthak Vashisth <sarthakvashisth@yahoo.com>
2025-05-13 06:50:19 +0530

    Swapping Nodes kth from start and end

--- DataStructures/LinkedList/swapping_nodes.py
+++ DataStructures/LinkedList/swapping_nodes.py
@@ -1,3 +1,9 @@
+"""
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+"""
+
 from typing import Generic, TypeVar, Optional
 
 T  = TypeVar('T')
@@ -26,26 +32,40 @@ class LinkedList():
                 current = current.next
             current.next = new_node
 
-    def remove_nth_from_end(self, pos:int):
+    def swap(self, pos):
+        """kth-node from beginning and len-kth node from beginning"""
         len = 1
         current = self.head
-
         while current.next:
             len+=1
             current = current.next
+        # print(len)
         if len == pos:
             self.head = None
+        elif pos == int(len/2) + 1 and (len%2 != 0):
+            # print(pos, len)
+            return
         else:
-            count = 0
-            pos_from_start = len - pos
-            prev = current
-            current = self.head
-            while count < pos_from_start:
-                prev = current
-                current = current.next
-                count+=1
-            prev.next = current.next
-            current.next = None
+            # print(len)
+            count_kth = 1
+            count_len_kth = 1
+            kth_node_pos_from_end = len - pos
+            kth_node = self.head
+            len_kth_node = self.head
+            while count_kth < pos:
+                count_kth +=1
+                kth_node = kth_node.next
+            # print(kth_node.data)
+            while count_len_kth <= kth_node_pos_from_end:
+                count_len_kth += 1
+                len_kth_node = len_kth_node.next
+            # print(pos)
+            # print(count_kth)
+            # print(count_len_kth)
+            kth_node.data, len_kth_node.data = len_kth_node.data, kth_node.data 
+
+
+
 
     def print(self):
         current=self.head
@@ -62,9 +82,8 @@ if __name__ == "__main__":
     l1.insert("Sarthak")
     l1.insert("Great")
     l1.insert("Hi")
+    l1.insert("HeHe")
+    l1.insert("HaHa")
     l1.print()
-    l1.remove_nth_from_end(3)
+    l1.swap(2)
     l1.print()
-
-        
-
